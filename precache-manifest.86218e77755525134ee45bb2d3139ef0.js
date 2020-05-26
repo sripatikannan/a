47f35c9c3e1753501f8c5e258cbccc9a92f329fe
root <sripatikannan@gmail.com>
2020-05-25 22:03:07 -0500

    deploy

---
+++ precache-manifest.86218e77755525134ee45bb2d3139ef0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e208ff38c94d0f127fb",
+    "url": "sripatikannan.github.io/css/app.b7611bc6.css"
+  },
+  {
+    "revision": "6046cfccbc5dac1cd0004b5889b44a58",
+    "url": "sripatikannan.github.io/fonts/fira-sans-condensed-v4-latin-900italic.6046cfcc.woff2"
+  },
+  {
+    "revision": "8b02a97e476f0afe453515190e2ae904",
+    "url": "sripatikannan.github.io/fonts/fira-sans-condensed-v4-latin-900italic.8b02a97e.woff"
+  },
+  {
+    "revision": "f6cc5fd529fa2ea496175a9bd9303f97",
+    "url": "sripatikannan.github.io/img/download.f6cc5fd5.png"
+  },
+  {
+    "revision": "9030e6f279466888433359ca8c3ce28d",
+    "url": "sripatikannan.github.io/index.html"
+  },
+  {
+    "revision": "2e208ff38c94d0f127fb",
+    "url": "sripatikannan.github.io/js/app.5438e9c8.js"
+  },
+  {
+    "revision": "ac9658d0fd0966e29dbf",
+    "url": "sripatikannan.github.io/js/chunk-vendors.c0c71387.js"
+  },
+  {
+    "revision": "990171a200951e6cbdbb65704f09ab37",
+    "url": "sripatikannan.github.io/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "sripatikannan.github.io/robots.txt"
+  }
+]);
